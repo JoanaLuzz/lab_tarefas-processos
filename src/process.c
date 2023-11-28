81b4da0f28fce64d1b34896cff2a98fabe5506c2
Joana Luz <joana.luz.remelhe@tecnico.ulisboa.pt>
2023-11-28 15:40:55 +0000

    lab

--- src/process.c
+++ src/process.c
@@ -4,27 +4,33 @@
 
 #include <pthread.h>
 #include <stdio.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 /* global value */
 int g_value = 0;
 
-void* thr_func(void* p) {
+void* child_func() {
     g_value = 1;
     return NULL;
 }
 
 int main() {
-    pthread_t tid;
-    if (pthread_create(&tid, NULL, thr_func, NULL) != 0) {
-        fprintf(stderr, "error creating thread.\n");
+    size_t pid = fork();
+    if (pid == -1) {
         return -1;
     }
 
-    g_value = 2;
+    if (pid) {
+        g_value = 2;
 
-    if (pthread_join(tid, NULL) != 0) {
-        fprintf(stderr, "error joining thred.\n");
-        return -1;
+        if (waitpid(pid, NULL, 0) == -1) {
+            return -1;
+        }
+    }
+
+    else {
+        child_func();
     }
 
     printf("value = %d\n", g_value);
